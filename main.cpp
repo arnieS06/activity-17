4ab71beabd7b35967a464115f3058566b657e58f
arnieS06 <sharma.arnav2016@gmail.com>
2024-10-07 10:04:26 -0700

    done

--- main.cpp
+++ main.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+
+    int arrayNumbers[] = {24, 4, 6, 99, 10, 23};
+    int size = sizeof(arrayNumbers)/sizeof(int);
+    int desiredNum = 99;
+
+    for (int i = 0; i < size; i++) {
+        if (i = desiredNum) {
+            cout << "Element found! it was " << i;
+        }
+        else {
+            continue;
+        }
+    }
+}
