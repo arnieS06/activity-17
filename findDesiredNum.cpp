49433e78a16c1313de82504f257cf7bde24e2e9f
arnieS06 <sharma.arnav2016@gmail.com>
2024-11-17 23:25:43 -0800

    Update findDesiredNum.cpp

--- findDesiredNum.cpp
+++ findDesiredNum.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
+#include "findDesiredNum.hpp"
 
-int findDesiredNum(int arrayNumbers[], int desiredNum, int arraySize) {
-    
-    for (int n = 0; n < arraySize; n++) {
-        if (arrayNumbers[n] == desiredNum) {
-            std::cout << "\nYour number exists at index ";
-            return n;
+namespace search {  // Wrap the function inside the search namespace
+    int findDesiredNum(int arrayNumbers[], int desiredNum, int arraySize) {
+        for (int n = 0; n < arraySize; n++) {
+            if (arrayNumbers[n] == desiredNum) {
+                std::cout << "\nYour number exists at index ";
+                return n;
+            }
         }
 
+        std::cout << "\nYour number doesn't exist in this array." << std::endl;
+        return -1;
     }
-
-    std::cout << "\nYour number doesn't exist in this array." << std::endl;
-    return -1;
-
 }
