a37766ced1432471908d5ff4767087a253eff778
arnieS06 <sharma.arnav2016@gmail.com>
2024-11-17 23:25:13 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include "findDesiredNum.hpp"
 using namespace std;
+using namespace search; 
 
 int main() {
-
     int arrayNumbers[] = {24, 4, 6, 99, 10, 23, 405, 298, 29, 9};
-    int size = sizeof(arrayNumbers)/sizeof(int);
+    int size = sizeof(arrayNumbers) / sizeof(int);
     int desiredNum;
 
     cout << "There's an integer array. Give me a number that you want to find in it. I'll tell you if it exists in there. -> ";
@@ -14,5 +14,4 @@ int main() {
     cout << "Here's what I found: " << findDesiredNum(arrayNumbers, desiredNum, size);
 
     return 0;
-
 }
